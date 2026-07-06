50cf364d0c30c2c7d58d940d42b5245be8260142
Nilmoni Pangas <155773179+Nilmoni04@users.noreply.github.com>
2026-07-07 01:15:55 +0530

    Time: 6 ms (99.28%), Space: 40.9 MB (99.95%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,50 +1,80 @@
-class Solution {
+public class Solution {
+    
     public void solveSudoku(char[][] board) {
-        solve(board, 0, 0);
+        solve(board);
     }
-    private boolean solve(char[][] board, int row, int col) {
-        if(row == 9) return true;
-        int nextRow=row, nextCol=col+1;
-        if(nextCol == 9) {
-            nextRow = row+1;
-            nextCol = 0;
+    
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                // Find an empty cell
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num; // Place the number
+                            if (solve(board)) {
+                                return true; // If solved, return true
+                            }
+                            board[row][col] = '.'; // Backtrack
+                        }
+                    }
+                    return false; // If no number can be placed, return false
+                }
+            }
+        }
+        return true; // All cells are filled correctly
+    }
+    
+    private boolean isSafe(char[][] board, int row, int col, char num) {
+        // Check row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
         }
 
-        if(board[row][col] != '.') {
-            return solve(board, nextRow, nextCol);
-        }
-        for(char dig='1'; dig<='9'; dig++) {
-            if(isSafe(board, row, col, dig)) {
-                board[row][col] = dig;
-                
-            if(solve(board, nextRow, nextCol)) {
-                return true;
-            }
-            board[row][col]='.';
-            }
-        }
-        return false;
-    }
-    private boolean isSafe(char[][] board, int row, int col, char dig) {
-        for(int j=0; j<9; j++) {
-            if(board[row][j] == dig) {
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
                 return false;
             }
         }
-        for(int i=0; i<9; i++) {
-            if(board[i][col] == dig) {
-                return false;
-            }
-        }
-        int srow=(row/3)*3;
-        int scol=(col/3)*3;
-        for(int i=srow; i<=srow+2; i++) {
-            for(int j=scol; j<=scol+2; j++) {
-                if(board[i][j] == dig) {
+
+        // Check 3x3 sub-box
+        int boxRowStart = (row / 3) * 3;
+        int boxColStart = (col / 3) * 3;
+        for (int i = boxRowStart; i < boxRowStart + 3; i++) {
+            for (int j = boxColStart; j < boxColStart + 3; j++) {
+                if (board[i][j] == num) {
                     return false;
                 }
             }
         }
-        return true;
+        return true; // It's safe to place the number
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        Solution solver = new Solution();
+        solver.solveSudoku(board);
+        
+        // Print the solved Sudoku board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
