0ea1130a3838070cad7a2ab13ba928ac762f9e91
Nilmoni Pangas <155773179+Nilmoni04@users.noreply.github.com>
2026-09-21 18:28:28 +0530

    Time: 84 ms (100%), Space: 239 MB (100%) - LeetHub

---
+++ 4057-number-of-intersecting-interval-pairs-ii/4057-number-of-intersecting-interval-pairs-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countIntersectingIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long pairs = 0;
+        for(int[] interval : intervals) {
+            int st = interval[0];
+            int end = interval[1];
+
+            while(!minHeap.isEmpty() && minHeap.peek() < st) {
+                minHeap.poll();
+            }
+            pairs+=minHeap.size();
+            minHeap.offer(end);
+        }
+        return pairs;
+    }
+}
